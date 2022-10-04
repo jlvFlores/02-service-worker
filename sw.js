240ca935c1566ca62b96a4f570410210090ee681
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-03 19:34:51 -0500

    39. Modificando la respuesta de la peticion Fetch

--- sw.js
+++ sw.js
@@ -2,11 +2,19 @@
 
 self.addEventListener('fetch', event => {
 
-    if ( event.request.url.includes('.jpg') ) {
-        console.log(event.request.url);
+    if ( event.request.url.includes('style.css') ) {
 
-        let fotoReq = fetch( event.request );
+        let respuesta = new Response(`
+            body {
+                background-color: red !important;
+                color: pink;
+            }
+        `, {
+            headers: {
+                'Content-type': 'text/css'
+            }
+        });
 
-        event.respondWith( fotoReq );
+        event.respondWith( respuesta );
     }
 });
